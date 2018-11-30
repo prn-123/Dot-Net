04f6a21192256aaca1899d68b6df259554d75e6b
prn-123 <pournamirnair9@gmail.com>
2018-11-30 10:33:01 +0530

    Commit-7

---
+++ Nov-28/EFContactManagement/EFContactManagement/Controllers/GroupController.cs
@@ -0,0 +1,23 @@
+﻿using EFContactManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFContactManagement.Controllers
+{
+    public class GroupController : Controller
+    {
+        ContactManagementSystemEntities ctx = new ContactManagementSystemEntities();
+        // GET: Group
+        public ActionResult Index()
+        {
+            return View(ctx.ContactGroups.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nov-28/EFContactManagement/EFContactManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFContactManagement.MvcApplication" Language="C#" %>
