ce9851f3816acd45ac07efefc83f44b6c5b84b18
prn-123 <pournamirnair9@gmail.com>
2018-11-30 10:31:20 +0530

    Commit-6

---
+++ Nov-28/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8000";
+            using (WebApp.Start<StartUp>(uri))
+            {
+                Console.WriteLine("Hello World!");
+                Console.ReadKey();
+            }
+        }
+    }
+    public class StartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx =>
+            {
+                return ctx.Response.WriteAsync("Hello World");
+            });
+        }
+    }
+}
